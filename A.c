f0d5e71fa66348c87ac90340356cb7aac953baac
petterxiaz <131241546+petterxiaz@users.noreply.github.com>
2024-01-23 18:57:26 +0800

    上传文件
    第一次上传github文件

---
+++ A.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	signed int a = -8;
+	unsigned int b = 7;
+	int c;
+	c = (3 < 5) ? (8):(9);
+	printf("%x\n", a + b);
+	printf("%d\n", -5 & 10);
+	printf("%d\n", (-1) >> 3);
+	printf("%d\n",c );
+
+	return 0;
+
+
+
+
+
+
+
+}
